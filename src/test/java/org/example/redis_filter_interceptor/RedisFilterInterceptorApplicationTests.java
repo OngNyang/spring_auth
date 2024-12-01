a8995ac05971373e4a93895e710199d5c21fcf92
OngNyang <108539087+OngNyang@users.noreply.github.com>
2024-12-01 16:28:36 +0900

    Initial commit

---
+++ src/test/java/org/example/redis_filter_interceptor/RedisFilterInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.redis_filter_interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisFilterInterceptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
